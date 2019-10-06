bacec32dc74697db207ce393e78c9e9d7ebbd13b
Kyparos <sasha.malakhatka@gmail.com>
2019-10-06 18:45:54 +0300

    labaratory #3 1.1

--- labaratory3/labar3.1.py
+++ labaratory3/labar3.1.py
@@ -1,11 +1,15 @@
-import logging
+import logging, Checker
 logging.basicConfig(level=logging.WARN)
-words = []
+Checker.greet(3,'Замінити перші 3 символи слів, що мають обрану довжину, на символ')
 while True:
-    words.append(input('Введить слово'))
+    words = []
+    while True:
+        words.append(input('Введить слово'))
+        if input('Бажаєте продовжити(+)') != '+':
+            break
+    logging.debug(words)
+    for i in range(len(words)):
+        words[i] = '***'+words[i][3:]
+    print(words)
     if input('Бажаєте продовжити(+)') != '+':
-        break
-logging.debug(words)
-for i in range(len(words)):
-    words[i] = '***'+words[i][3:]
-print(words)
+            break

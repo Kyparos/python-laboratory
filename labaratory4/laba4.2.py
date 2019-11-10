a9ec5cb4ce7d2fde42f8814656fef9f66b401b7a
Kyparos <sasha.malakhatka@gmail.com>
2019-11-10 18:13:56 +0200

    laba 5.1

--- labaratory4/laba4.2.py
+++ labaratory4/laba4.2.py
@@ -6,20 +6,20 @@ boredr_check = re.compile('^\s{%d}' % border)
 
 
 def left(s, len_border=1):
-    border = len_border+1
+    border = len_border + 1
     while bool(boredr_check.match(s)):
         s = s[1:]
     if s[0] != ' ':
-        s = ' '*len_border + s
+        s = ' ' * len_border + s
     return s
 
 
 ch.greet(4, '7) Функція - Left(s,l). Призначення - вирівнювання рядка s по лівому краю до довжини l.')
 while True:
     text = input('Введіть текст: ')
-    print('Введіть довжинну відступпу:',end=' ')
+    print('Введіть довжинну відступпу:', end=' ')
     len = ch.nCheck()
-    text = left(text,len)
+    text = left(text, len)
     print(text)
     if input('Бажаєте продовжити(+)') != '+':
         break
---
+++ labrotory5/laba5.1.py
@@ -0,0 +1,23 @@
+import Checker as ch
+import re
+
+
+def pars(lis, separ='0'):
+    mainTemp = []
+    temp = []
+    for i in range(len(lis)):
+        if lis[i] != separ: temp.append(lis[i])
+        else:
+            mainTemp.append(temp)
+            temp = []
+    mainTemp.append(temp)
+    return mainTemp
+
+
+while True:
+    ch.greet(1, 'Перетворити однорівневий список у список із вкладеними списками, згідно з визначеними умовами. ')
+    rawList = input('Введіть елемнти списку через пробіл з родільником "0" ').split(' ')
+    newList = pars(rawList)
+    print(newList)
+    if input('Бажаєте продовжити(+)') != '+':
+        break

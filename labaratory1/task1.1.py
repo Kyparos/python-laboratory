1e461f8f30ab3b60eae2e36ee3c262db68cc6ade
Kyparos <sasha.malakhatka@gmail.com>
2019-11-11 20:37:50 +0200

    more validators

--- labaratory1/task1.1.py
+++ labaratory1/task1.1.py
@@ -4,9 +4,21 @@
 (REGex  у Сhecker.py)
 """
 
-
-
 import Checker as ch
+import re
+
+
+def fSValid(x):
+    """
+    check if x is  1 or 2
+    :param x: any
+    :return: bool
+    """
+    if bool(re.fullmatch('\s*[12]\s*',x)):
+        return True
+    else:
+        return False
+
 
 ch.greet(1, """Користувач уводить суму вкладу в банк і річний відсоток.
 Знайти суму вкладу через 5 років (розглянути два способи нарахування відсотків)
@@ -26,15 +38,13 @@ print("""Оберіть тип капіталізаціі
  1) Без капіталізаціі 
  2) Шорічна капіталізаціі """)
 ans = input()
-while True:
-    ans = ch.floatCheck(ans)
-    if ans == 1 or ans == 2:
-        break
-    else:
-        print(m2)
+while not (fSValid(ans)):
+    print(m2)
+    ans = input()
+ans = int(ans)
 if ans == 1:
     summ = (5 * pro * summ) + summ
 else:
     summ = (((pro + 1) ** 5) * summ)
 print("Ваш вклад через 5 років:%.2f" % (summ))
-input()
+
